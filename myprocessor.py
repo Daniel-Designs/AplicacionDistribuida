985d4d4faaa3b3b66ecee27a07750ac3e3e64edf
Daniel <tonystar.dm@gmail.com>
2021-12-09 16:45:20 -0600

    Prueba Funcionalidad de Listas productos

--- myprocessor.py
+++ myprocessor.py
@@ -5,16 +5,7 @@ productos = [{'ID': '1','NAME':'Producto1'},
                 {'ID': '1','NAME':'Producto1'},
                 {'ID': '1','NAME':'Producto1'},
                 {'ID': '1','NAME':'Producto1'}]
-
-
-
-
-   ''' 
-    def pagaProducto(self,ID):
-
-    def compraArticulo(self,ID): 
-   '''
-
+                
     def listarProductos(self):
         return productos
 
